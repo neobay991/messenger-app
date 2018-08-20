77593bde3458f16e9e0976389903e01fd2a24aed
Jay Khan <jkhan992@gmail.com>
2018-08-20 14:19:24 +0100

    Initial Commit

---
+++ messenger-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  messenger-app
+//
+//  Created by M Jawad Khan on 20/08/2018.
+//  Copyright © 2018 Jay Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
