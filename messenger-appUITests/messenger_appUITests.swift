f1a9f665ecb3740b74c11470b8a57d5ef83c3700
Jay Khan <jkhan992@gmail.com>
2018-08-22 23:14:41 +0100

    TESTS Add Feature test to check for login to failure

--- messenger-appUITests/messenger_appUITests.swift
+++ messenger-appUITests/messenger_appUITests.swift
@@ -50,7 +50,7 @@ class messenger_appUITests: messenger_appUITestsCase {
         XCTAssert(toolbar.buttons["Send"].exists)
     }
  
-    func testLoginIn() {
+    func testLoginInSuccess() {
         
         app/*@START_MENU_TOKEN@*/.buttons["Log In"]/*[[".segmentedControls.buttons[\"Log In\"]",".buttons[\"Log In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
@@ -67,6 +67,24 @@ class messenger_appUITests: messenger_appUITestsCase {
         let toolbar = app.toolbars["Toolbar"]
         toolbar.textViews["New Message"].tap()
         XCTAssert(toolbar.buttons["Send"].exists)
+    }
+    
+    func testLoginInFail() {
+        
+        app/*@START_MENU_TOKEN@*/.buttons["Log In"]/*[[".segmentedControls.buttons[\"Log In\"]",".buttons[\"Log In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let emailTextField = app.textFields["Email"]
+        emailTextField.tap()
+        emailTextField.typeText("test@test.com")
+        
+        let passwordTextField = app.textFields["Password"]
+        passwordTextField.tap()
+        passwordTextField.typeText("wromngpassword")
+        
+        app.buttons["OK"].tap()
+        
+        XCTAssert(app.buttons["OK"].exists)
+
     }
     
     func testNewUserCanPost() {
--- messenger-appUITests/messenger_appUITestsCase.swift
+++ messenger-appUITests/messenger_appUITestsCase.swift
@@ -33,21 +33,4 @@ class messenger_appUITestsCase: XCTestCase {
         app.terminate()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-//    
-//    func waitForElementToAppear(_ element: XCUIElement, file: String = #file, line: UInt = #line) {
-//        let existsPredicate = NSPredicate(format: "exists == true")
-//        expectation(for: existsPredicate, evaluatedWith: element, handler: nil)
-//        
-//        waitForExpectations(timeout: 5) { (error) -> Void in
-//            if (error != nil) {
-//                let message = "Failed to find \(element) after 5 seconds."
-//                self.recordFailure(withDescription: message, inFile: file, atLine: line, expected: true)
-//            }
-//        }
-//    }
-    
 }
