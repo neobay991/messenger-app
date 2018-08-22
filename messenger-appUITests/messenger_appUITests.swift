79d325e19691d891f6bf3da98d0c653420f667b8
Jay Khan <jkhan992@gmail.com>
2018-08-22 22:58:24 +0100

    TESTS Add Feature test to check an existing user can post a message

--- messenger-appUITests/messenger_appUITests.swift
+++ messenger-appUITests/messenger_appUITests.swift
@@ -92,4 +92,23 @@ class messenger_appUITests: messenger_appUITestsCase {
         toolbar.buttons["Send"].tap()
     }
     
+    func testExistingUserCanPost() {
+        
+        app/*@START_MENU_TOKEN@*/.buttons["Log In"]/*[[".segmentedControls.buttons[\"Log In\"]",".buttons[\"Log In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let emailTextField = app.textFields["Email"]
+        emailTextField.tap()
+        emailTextField.typeText("test@test.com")
+        
+        let passwordTextField = app.textFields["Password"]
+        passwordTextField.tap()
+        passwordTextField.typeText("password")
+        
+        app.buttons["OK"].tap()
+        
+        let toolbar = app.toolbars["Toolbar"]
+        toolbar.textViews["New Message"].tap()
+        toolbar.typeText("Test message")
+        toolbar.buttons["Send"].tap()
+    }
 }
