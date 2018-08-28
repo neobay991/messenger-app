128a80708589e1285fcdeaaab80efb844bfd2182
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-28 19:04:17 +0100

    Move navigation controller to entry point

--- messenger-app/UserViewController.swift
+++ messenger-app/UserViewController.swift
@@ -54,4 +54,14 @@ class UserViewController: UIViewController {
             }
         })
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
 }
