46ee577e533dfd9ed11c9829fc97339edaa14084
Jay Khan <jkhan992@gmail.com>
2018-08-29 15:28:01 +0100

    OTHER REFACTOR Extracted SignUp function into SignUpViewController class

--- messenger-app/UserViewController.swift
+++ messenger-app/UserViewController.swift
@@ -16,28 +16,28 @@ class UserViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     
-    @IBAction func signUp(_ sender: UIButton) {
-        let username = usernameText.text!
-        let email = emailText.text!
-        let password = passwordText.text!
-        database.createUser(withEmail: email, password: password, completion: { (user, error) in
-            if user != nil {
-                let changeRequest = self.database.currentUser?.createProfileChangeRequest()
-                changeRequest?.displayName = username
-                changeRequest?.commitChanges { (error) in
-                    if error != nil {
-                        print(error!.localizedDescription)
-                    }
-                }
-                self.performSegue(withIdentifier: "Sign up successful", sender: self)
-            } else {
-                let alertMessage = error?.localizedDescription ?? "Please enter a valid email address an password"
-                let alert = UIAlertController(title: "Failed to sign up", message: alertMessage, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                self.present(alert, animated: true)
-            }
-        })
-    }
+//    @IBAction func signUp(_ sender: UIButton) {
+//        let username = usernameText.text!
+//        let email = emailText.text!
+//        let password = passwordText.text!
+//        database.createUser(withEmail: email, password: password, completion: { (user, error) in
+//            if user != nil {
+//                let changeRequest = self.database.currentUser?.createProfileChangeRequest()
+//                changeRequest?.displayName = username
+//                changeRequest?.commitChanges { (error) in
+//                    if error != nil {
+//                        print(error!.localizedDescription)
+//                    }
+//                }
+//                self.performSegue(withIdentifier: "Sign up successful", sender: self)
+//            } else {
+//                let alertMessage = error?.localizedDescription ?? "Please enter a valid email address an password"
+//                let alert = UIAlertController(title: "Failed to sign up", message: alertMessage, preferredStyle: .alert)
+//                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+//                self.present(alert, animated: true)
+//            }
+//        })
+//    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
