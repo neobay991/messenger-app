35a478f036c89d6842edf58a083640033d049882
Jay Khan <jkhan992@gmail.com>
2018-08-29 14:59:53 +0100

    OTHER Fix failing tests

--- messenger-appUITests/messenger_appUITests.swift
+++ messenger-appUITests/messenger_appUITests.swift
@@ -12,7 +12,7 @@ class messengerAppUITests: messengerAppUITestsCase {
     
     // It checks the sign up form exists
     func testSignUpFormExists() {
-        app.buttons["Sign up now"].tap()
+        app.buttons["Sign up"].tap()
         
         XCTAssert(app.textFields["Username"].exists)
         XCTAssert(app.textFields["Email"].exists)
@@ -39,18 +39,22 @@ class messengerAppUITests: messengerAppUITestsCase {
         
         app.buttons["Log in"].tap()
         
-        waitForElementToAppear(app.buttons["Channel 1"])
-        waitForElementToAppear(app.buttons["Channel 2"])
-        waitForElementToAppear(app.buttons["Channel 3"])
+        waitForElementToAppear(app.buttons["The Living Room"])
+        waitForElementToAppear(app.buttons["Tech Talk Zone"])
+        waitForElementToAppear(app.buttons["I ❤️ Meshenger"])
         
-        XCTAssert(app.buttons["Channel 1"].exists)
-        XCTAssert(app.buttons["Channel 2"].exists)
-        XCTAssert(app.buttons["Channel 3"].exists)
+        XCTAssert(app.buttons["The Living Room"].exists)
+        XCTAssert(app.buttons["Tech Talk Zone"].exists)
+        XCTAssert(app.buttons["I ❤️ Meshenger"].exists)
     }
-
+    
     // It checks a new user can successfully sign up
     func testSignUpSuccess() {
-        app.buttons["Sign up now"].tap()
+        app.buttons["Sign up"].tap()
+        
+        let usernameTextField = app.textFields["Username"]
+        usernameTextField.tap()
+        usernameTextField.typeText("test")
         
         // generate a random number to append to email address
         let randomNumber = arc4random()
@@ -65,13 +69,13 @@ class messengerAppUITests: messengerAppUITestsCase {
         
         app.buttons["Sign up"].tap()
         
-        waitForElementToAppear(app.buttons["Channel 1"])
-        XCTAssert(app.buttons["Channel 1"].exists)
+        waitForElementToAppear(app.buttons["The Living Room"])
+        XCTAssert(app.buttons["The Living Room"].exists)
     }
     
     // It checks a new user cannot sign up if they do not enter an email address
     func testSignUpFailEmailAddressMissing() {
-        app.buttons["Sign up now"].tap()
+        app.buttons["Sign up"].tap()
         
         let usernameTextField = app.textFields["Username"]
         usernameTextField.tap()
@@ -89,7 +93,7 @@ class messengerAppUITests: messengerAppUITestsCase {
     
     // It checks a new user cannot sign up if they enter an invalid email address
     func testSignUpFailEmailAddressInvalid() {
-        app.buttons["Sign up now"].tap()
+        app.buttons["Sign up"].tap()
         
         let usernameTextField = app.textFields["Username"]
         usernameTextField.tap()
@@ -113,7 +117,7 @@ class messengerAppUITests: messengerAppUITestsCase {
     
     // It checks a new user cannot sign up if they do not exter a password
     func testSignUpFailPasswordMissing() {
-        app.buttons["Sign up now"].tap()
+        app.buttons["Sign up"].tap()
         
         let usernameTextField = app.textFields["Username"]
         usernameTextField.tap()
@@ -147,8 +151,8 @@ class messengerAppUITests: messengerAppUITestsCase {
         
         app.buttons["Log in"].tap()
         
-        waitForElementToAppear(app.buttons["Channel 1"])
-        XCTAssert(app.buttons["Channel 1"].exists)
+        waitForElementToAppear(app.buttons["The Living Room"])
+        XCTAssert(app.buttons["The Living Room"].exists)
     }
     
     // It checks an existing user cannot log in if they enter wrong password
@@ -198,7 +202,7 @@ class messengerAppUITests: messengerAppUITestsCase {
     }
     
     func testNewUserCanPost() {
-        app.buttons["Sign up now"].tap()
+        app.buttons["Sign up"].tap()
         
         let usernameTextField = app.textFields["Username"]
         usernameTextField.tap()
@@ -217,7 +221,7 @@ class messengerAppUITests: messengerAppUITestsCase {
         
         app.buttons["Sign up"].tap()
         
-        app.buttons["Channel 1"].tap()
+        app.buttons["The Living Room"].tap()
         
         let toolbar = app.toolbars["Toolbar"]
         toolbar.textViews["New Message"].tap()
@@ -236,7 +240,7 @@ class messengerAppUITests: messengerAppUITestsCase {
         
         app.buttons["Log in"].tap()
         
-        app.buttons["Channel 1"].tap()
+        app.buttons["The Living Room"].tap()
         
         let toolbar = app.toolbars["Toolbar"]
         toolbar.textViews["New Message"].tap()
@@ -256,7 +260,7 @@ class messengerAppUITests: messengerAppUITestsCase {
         
         app.buttons["Log in"].tap()
         
-        app.buttons["Channel 1"].tap()
+        app.buttons["The Living Room"].tap()
         
         let toolbar = app.toolbars["Toolbar"]
         
@@ -264,17 +268,17 @@ class messengerAppUITests: messengerAppUITestsCase {
         toolbar.typeText("Test message")
         toolbar.buttons["Send"].tap()
         
-        app.navigationBars["messenger_app.ChatView"].buttons["Back"].tap()
+        app.navigationBars["The Living Room"].buttons["Channels"].tap()
         
-        app.buttons["Channel 2"].tap()
+        app.buttons["Tech Talk Zone"].tap()
         
         toolbar.textViews["New Message"].tap()
         toolbar.typeText("Test message")
         toolbar.buttons["Send"].tap()
         
-        app.navigationBars["messenger_app.ChatView"].buttons["Back"].tap()
+        app.navigationBars["Tech Talk Zone"].buttons["Channels"].tap()
         
-        app.buttons["Channel 3"].tap()
+        app.buttons["I ❤️ Meshenger"].tap()
         
         toolbar.textViews["New Message"].tap()
         toolbar.typeText("Test message")
